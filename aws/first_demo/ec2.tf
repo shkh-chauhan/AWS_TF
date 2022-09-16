157e07e4b39789ec39f15e4d749905b9e4c56b33
shkh-chauhan <59258019+shkh-chauhan@users.noreply.github.com>
2022-09-16 11:07:58 +0530

    Update ec2.tf

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "myawsserver" {
   instance_type = "t2.nano"
 
   tags = {
-    Name = "shikha-TF_Cloud_VersionControlledWF1"
+    Name = "shikha-vm_tf_Cloud"
     Env = "test"
     Owner = "shikha"
   }
